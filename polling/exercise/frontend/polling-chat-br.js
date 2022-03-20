e533f1f38e3b1e6f070a45306335857c843392f8
Rebeca Diaconu <rebeca.diaconu@tremend.com>
2022-03-20 23:49:05 +0200

    Added backoff and retry logic

---
+++ polling/exercise/frontend/polling-chat-br.js
@@ -0,0 +1,80 @@
+const chat = document.getElementById("chat");
+const msgs = document.getElementById("msgs");
+
+// let's store all current messages here
+let allChat = [];
+let timeBeforeNextCall = 0;
+let failedRequests = 0;
+
+// the interval to poll at in milliseconds
+const INTERVAL = 3000;
+const BACKOFF = 5000;
+
+// a submit listener on the form in the HTML
+chat.addEventListener("submit", function (e) {
+  e.preventDefault();
+  postNewMsg(chat.elements.user.value, chat.elements.text.value);
+  chat.elements.text.value = "";
+});
+
+async function postNewMsg(user, text) {
+  const payload = {
+    user,
+    text,
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(payload),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  await fetch("/poll", options);
+};
+
+async function getNewMsgs() {
+  let messages;
+  try {
+    let response = await fetch("/poll");
+    messages = await response.json();
+
+    if (response.status > 400) {
+      throw new Error("request did not succeed: " + res.status);
+    }
+
+    allChat = messages.msg;
+    render();
+    failedRequests = 0;
+  } catch (e) {
+    console.error('polling error', e);
+    failedRequests++;
+  }
+}
+
+function render() {
+  // as long as allChat is holding all current messages, this will render them
+  // into the ui. yes, it's inefficent. yes, it's fine for this example
+  const html = allChat.map(({ user, text, time, id }) =>
+    template(user, text, time, id)
+  );
+  msgs.innerHTML = html.join("\n");
+}
+
+// given a user and a msg, it returns an HTML string to render to the UI
+const template = (user, msg) =>
+  `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
+
+// time parameter is provided back from the requestAnimationFrame function
+async function rafTimer(time) {
+  if (timeBeforeNextCall <= time) {
+    await getNewMsgs();
+    timeBeforeNextCall = time + INTERVAL + failedRequests * BACKOFF; // linear backoff (not exp)
+  }
+
+  requestAnimationFrame(rafTimer);
+};
+
+// make the first request
+requestAnimationFrame(rafTimer);
