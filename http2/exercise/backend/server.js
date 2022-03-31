8564da76d4a60e98a7c99b6b00bdee85217e7f91
Rebeca Diaconu <rebeca.diaconu@tremend.com>
2022-03-31 14:11:05 +0300

    Finish http2 push exercise

--- http2/exercise/backend/server.js
+++ http2/exercise/backend/server.js
@@ -29,11 +29,27 @@ const server = http2.createSecureServer({
   key: fs.readFileSync(path.join(__dirname, "/../key.pem")),
 });
 
-/*
- *
- * Code goes here
- *
- */
+server.on("stream", (stream, headers) => {
+  const path = headers[":path"];
+  const method = headers[":method"];
+
+  console.log('Connected', stream.id);
+
+  if (path === '/msgs' && method === "GET") {
+    stream.respond({
+      "status": 200,
+      "content-type": "text/plain; charset=utf-8",
+    });
+
+    stream.write(JSON.stringify({ msgs: getMsgs() }));
+    connections.push(stream);
+
+    stream.on("close", () => {
+      console.log('Stream disconnected', stream.id);
+      connections = connections.filter((s) => s !== stream);
+    });
+  }
+});
 
 server.on("request", async (req, res) => {
   const path = req.headers[":path"];
@@ -52,12 +68,16 @@ server.on("request", async (req, res) => {
     }
     const data = Buffer.concat(buffers).toString();
     const { user, text } = JSON.parse(data);
+    msg.push({
+      user,
+      text,
+      time: Date.now(),
+    });
 
-    /*
-     *
-     * some code goes here
-     *
-     */
+    res.end();
+    connections.forEach((stream) => {
+      stream.write(JSON.stringify({ msg: getMsgs() }));
+    });
   }
 });
 
